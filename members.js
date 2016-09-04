8e71b48bd91cfd9edca876d2f08c34500f5006f3
Ashley Wang <ashleywang1@yahoo.com>
2016-09-04 12:16:08 -0400

    fix tabletop position

--- members.js
+++ members.js
@@ -1,16 +1,14 @@
-<script type="text/javascript">
-  window.onload = function() { init() };
+window.onload = function() { init() };
 
-  var public_spreadsheet_url = 'https://docs.google.com/spreadsheets/d/1lqy9W2N2y7tbqt04-wJVrdz6cC1gkR3njkuiP2_bbLg/pubhtml';
+var public_spreadsheet_url = 'https://docs.google.com/spreadsheets/d/1lqy9W2N2y7tbqt04-wJVrdz6cC1gkR3njkuiP2_bbLg/pubhtml';
 
-  function init() {
-    Tabletop.init( { key: public_spreadsheet_url,
-                     callback: showInfo,
-                     simpleSheet: true } )
-  }
+function init() {
+  Tabletop.init( { key: public_spreadsheet_url,
+                   callback: showInfo,
+                   simpleSheet: true } )
+}
 
-  function showInfo(data, tabletop) {
-    alert("Successfully processed!")
-    console.log(data);
-  }
-</script>
+function showInfo(data, tabletop) {
+  alert("Successfully processed!")
+  console.log(data);
+}
