401884c59b05456a208e62ba799c12535f1096f9
Ashley Wang <ashleywang1@yahoo.com>
2016-09-07 14:00:24 -0400

    smaller slides

--- members.js
+++ members.js
@@ -11,9 +11,9 @@ function init() {
 function showInfo(data, tabletop) {
   var rows = "";
   $.each(data, function(){
-      rows += "<tr><td align=\"center\" width=\"50%\"><img src=\"" + this.Picture + 
-      "\" style=\"height: 200px\"><br><b>" + this.Name + "</b><br>" 
-      + this.Course + "<br>" + this.Bio + "</td></tr>";
+      rows += "<tr><td align=\"center\"><img src=\"" + this.Picture +
+      "\" style=\"height: 200px\"><br><b>" + this.Name + "</b><br>"
+      + this.Course + "<br></td><td>" + this.Bio + "</td></tr>";
   });
 
   $( rows ).appendTo( "#memberTable tbody" );
